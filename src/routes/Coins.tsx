7bc5714c8345f3769bbabc9a689b89a968fa557e
Junyong-Ahn <junyongsw92@gmail.com>
2023-10-25 23:38:21 +0900

    #Home part One
    - App.tsx : css 추가
    - index.tsx : React.StringMode -> div 변경(rendering 안되는 문제.)
    - Coins.tsx
       -coin list 추가

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,82 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+const Header = styled.div`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  /* padding: 20px; */
+  /* border-radius: 15px; */
+  a {
+    transition: color 0.2s ease-in;
+    display: block; // link 글자 바깥까지 적용되게 설정
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor}; // 마우스 올라가면 색깔 전혼
+    }
+  }
 `;
 
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 function Coins() {
-  return <Title>Coins</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      {/* map 함수를 이용해서 coins list 를 다 보여줌  */}
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 }
 
 export default Coins;
