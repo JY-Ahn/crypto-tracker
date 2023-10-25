6317e5ba2fa0b8312e03a35878ecaa8132a20103
Junyong-Ahn <junyongsw92@gmail.com>
2023-10-25 22:49:58 +0900

    Global CSS 적용
    - global style 적용( createGlobalStyle  사용)
    - theme 적용 (Coin, Coins에 props 적용)

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,15 +1,18 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 interface RouteParams {
   coinId: string;
 }
+const Text = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
 function Coin() {
   //userParams: URL에서 필요한 정보를 잡아낼 수 있는 함수
   // string 타입인 coinId를 가지는 parameter를 userParams()가 반환할 것이라는걸 알려줌
-  const coinId = useParams<RouteParams>();
-  console.log(coinId);
+  const params = useParams<RouteParams>();
 
-  return <h1>Coin: {coinId.coinId}</h1>;
+  return <Text>Coin: {params.coinId}</Text>;
 }
 
 export default Coin;
