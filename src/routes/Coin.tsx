345b15438ebb76547e858e4bcb86717d76fdda84
Junyong-Ahn <junyongsw92@gmail.com>
2023-10-30 13:06:56 +0900

    Update Coin.tsx
    Api 에서 Coin data (info, price) 받아오기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -31,13 +31,89 @@ const Loader = styled.span`
 interface RouteState {
   name: string;
 }
+// chrome 검서 창에서 console.log() -> 오른쪽클릭 (temp1,2에 저장)
+// Object.keys(temp1).join() 을 통해 key값 문자열로 한번에 가져오기
+// Object.values(temp1).map(v => typeof v).join() 을 통해 value의 type 들을 문자열로 한번에 가져오기
+// 커맨드: option+shift+i / command+d
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   //userParams: URL에서 필요한 정보를 잡아낼 수 있는 함수
   // string 타입인 coinId를 가지는 parameter를 userParams()가 반환할 것이라는걸 알려줌
-  // const { coinId } = useParams<RouteParams>();
+  const { coinId } = useParams<RouteParams>();
   const [loading, setLoading] = useState(true);
   const { state } = useLocation<RouteState>();
-  console.log(state.name);
+  const [coinInfo, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+
+  useEffect(() => {
+    // 즉시 실행될 함수 ()();
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
